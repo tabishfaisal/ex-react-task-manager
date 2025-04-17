c46598a85321641bc110ab170b1d3fe6560f23d0
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2025-04-17 19:15:25 +0200

    milestone 11 done

--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,9 +1,41 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
 import { GlobalContext } from '../Context/GlobalContext'
 import TaskRow from '../Components/TaskRow'
 
 const TaskList = () => {
-  const { tasks } = useContext(GlobalContext)
+  const { tasks } = useContext(GlobalContext);
+  const [sortBy,setSortBy] = useState('createdAt');
+  const [sortOrder,setSortOrder] = useState(1);
+
+  const sortingSymbol = sortOrder === 1 ? '↑' : '↓';
+
+  const handleSorting = (colonna) =>{
+  if(sortBy === colonna){
+    setSortOrder((sortOrder)=>sortOrder *-1)
+  } else{
+    setSortBy(colonna);
+    setSortOrder(1);
+  }
+ }
+
+ const sortedTask = useMemo(()=>{
+  return tasks.sort((a,b)=>{
+    let comparison;
+    if(sortBy === 'title'){
+      comparison = a.title.localeCompare(b.title);
+    } else if(sortBy === 'createdAt'){
+      const dateA = new Date(a.createdAt).getTime();
+      const dateB = new Date(b.createdAt).getTime();
+      comparison = dateA-dateB;
+    } else if(sortBy === 'status'){
+      const options = ['to do','doing','done'];
+      const indexA = options.indexOf(a.status);
+      const indexB = options.indexOf(b.status);
+      comparison =  indexA - indexB;
+    }
+    return comparison * sortOrder;
+  })
+ },[tasks,sortBy,sortOrder])
 
   return (
     <div className='container'>
@@ -11,13 +43,19 @@ const TaskList = () => {
       <table>
         <thead>
           <tr>
-            <th>Nome</th>
-            <th>Created Date</th>
-            <th>Status</th>
+            <th onClick={()=>handleSorting('title')}>
+              Nome {sortBy === 'title' && sortingSymbol}
+              </th>
+            <th onClick={()=>handleSorting('createdAt')}>
+              Created Date {sortBy === 'createdAt' && sortingSymbol}
+              </th>
+            <th onClick={()=>handleSorting('status')}>
+              Status {sortBy === 'status' && sortingSymbol}
+              </th>
           </tr>
         </thead>
         <tbody>
-          {tasks.map((task) => (
+          {sortedTask.map((task) => (
             <TaskRow 
               key={task.id}
               id={task.id}
